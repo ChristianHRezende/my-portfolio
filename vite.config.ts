c06fc3b500faa71e64bf70e3eeaaf77ac1feaaa0
CHRISTIAN HENRIQUE REZENDE <christian_srs_@hotmail.com>
2025-08-04 19:13:08 +0000

    update pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/",
   test: {
     environment: "jsdom",
     globals: true,
